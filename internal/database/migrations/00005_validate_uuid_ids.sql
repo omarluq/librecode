6ef9031ef8560a0860b51dd10b04d6c4a1d00147
Omar Alani <omar.luqman@hey.com>
2026-05-29 17:58:00 -0500

    test(database): cover uuidv7 backfill paths

--- internal/database/migrations/00005_validate_uuid_ids.sql
+++ internal/database/migrations/00005_validate_uuid_ids.sql
@@ -1,9 +1,12 @@
 -- +goose Up
+CREATE TABLE IF NOT EXISTS uuid_v7_pattern (pattern TEXT NOT NULL);
+INSERT INTO uuid_v7_pattern (pattern) VALUES ('[0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F]-[0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F]-7[0-9a-fA-F][0-9a-fA-F][0-9a-fA-F]-[89aAbB][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F]-[0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F]');
+
 -- +goose StatementBegin
 CREATE TRIGGER IF NOT EXISTS validate_sessions_id_uuid_insert
 BEFORE INSERT ON sessions
 FOR EACH ROW
-WHEN NEW.id NOT GLOB '[0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F]-[0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F]-7[0-9a-fA-F][0-9a-fA-F][0-9a-fA-F]-[89aAbB][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F]-[0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F]'
+WHEN NEW.id NOT GLOB (SELECT pattern FROM uuid_v7_pattern LIMIT 1)
 BEGIN
     SELECT RAISE(ABORT, 'sessions.id must be a UUIDv7');
 END;
@@ -13,7 +16,7 @@ END;
 CREATE TRIGGER IF NOT EXISTS validate_sessions_id_uuid_update
 BEFORE UPDATE OF id ON sessions
 FOR EACH ROW
-WHEN NEW.id NOT GLOB '[0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F]-[0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F]-7[0-9a-fA-F][0-9a-fA-F][0-9a-fA-F]-[89aAbB][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F]-[0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F]'
+WHEN NEW.id NOT GLOB (SELECT pattern FROM uuid_v7_pattern LIMIT 1)
 BEGIN
     SELECT RAISE(ABORT, 'sessions.id must be a UUIDv7');
 END;
@@ -23,7 +26,7 @@ END;
 CREATE TRIGGER IF NOT EXISTS validate_sessions_parent_session_uuid_insert
 BEFORE INSERT ON sessions
 FOR EACH ROW
-WHEN NEW.parent_session <> '' AND NEW.parent_session NOT GLOB '[0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F]-[0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F]-7[0-9a-fA-F][0-9a-fA-F][0-9a-fA-F]-[89aAbB][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F]-[0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F]'
+WHEN NEW.parent_session <> '' AND NEW.parent_session NOT GLOB (SELECT pattern FROM uuid_v7_pattern LIMIT 1)
 BEGIN
     SELECT RAISE(ABORT, 'sessions.parent_session must be empty or a UUIDv7');
 END;
@@ -33,7 +36,7 @@ END;
 CREATE TRIGGER IF NOT EXISTS validate_sessions_parent_session_uuid_update
 BEFORE UPDATE OF parent_session ON sessions
 FOR EACH ROW
-WHEN NEW.parent_session <> '' AND NEW.parent_session NOT GLOB '[0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F]-[0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F]-7[0-9a-fA-F][0-9a-fA-F][0-9a-fA-F]-[89aAbB][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F]-[0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F]'
+WHEN NEW.parent_session <> '' AND NEW.parent_session NOT GLOB (SELECT pattern FROM uuid_v7_pattern LIMIT 1)
 BEGIN
     SELECT RAISE(ABORT, 'sessions.parent_session must be empty or a UUIDv7');
 END;
@@ -43,7 +46,7 @@ END;
 CREATE TRIGGER IF NOT EXISTS validate_session_entries_session_id_uuid_insert
 BEFORE INSERT ON session_entries
 FOR EACH ROW
-WHEN NEW.session_id NOT GLOB '[0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F]-[0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F]-7[0-9a-fA-F][0-9a-fA-F][0-9a-fA-F]-[89aAbB][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F]-[0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F]'
+WHEN NEW.session_id NOT GLOB (SELECT pattern FROM uuid_v7_pattern LIMIT 1)
 BEGIN
     SELECT RAISE(ABORT, 'session_entries.session_id must be a UUIDv7');
 END;
@@ -53,7 +56,7 @@ END;
 CREATE TRIGGER IF NOT EXISTS validate_session_entries_session_id_uuid_update
 BEFORE UPDATE OF session_id ON session_entries
 FOR EACH ROW
-WHEN NEW.session_id NOT GLOB '[0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F]-[0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F]-7[0-9a-fA-F][0-9a-fA-F][0-9a-fA-F]-[89aAbB][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F]-[0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F]'
+WHEN NEW.session_id NOT GLOB (SELECT pattern FROM uuid_v7_pattern LIMIT 1)
 BEGIN
     SELECT RAISE(ABORT, 'session_entries.session_id must be a UUIDv7');
 END;
@@ -63,7 +66,7 @@ END;
 CREATE TRIGGER IF NOT EXISTS validate_session_entries_id_uuid_insert
 BEFORE INSERT ON session_entries
 FOR EACH ROW
-WHEN NEW.id NOT GLOB '[0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F]-[0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F]-7[0-9a-fA-F][0-9a-fA-F][0-9a-fA-F]-[89aAbB][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F]-[0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F]'
+WHEN NEW.id NOT GLOB (SELECT pattern FROM uuid_v7_pattern LIMIT 1)
 BEGIN
     SELECT RAISE(ABORT, 'session_entries.id must be a UUIDv7');
 END;
@@ -73,7 +76,7 @@ END;
 CREATE TRIGGER IF NOT EXISTS validate_session_entries_id_uuid_update
 BEFORE UPDATE OF id ON session_entries
 FOR EACH ROW
-WHEN NEW.id NOT GLOB '[0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F]-[0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F]-7[0-9a-fA-F][0-9a-fA-F][0-9a-fA-F]-[89aAbB][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F]-[0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F]'
+WHEN NEW.id NOT GLOB (SELECT pattern FROM uuid_v7_pattern LIMIT 1)
 BEGIN
     SELECT RAISE(ABORT, 'session_entries.id must be a UUIDv7');
 END;
@@ -83,7 +86,7 @@ END;
 CREATE TRIGGER IF NOT EXISTS validate_session_entries_parent_id_uuid_insert
 BEFORE INSERT ON session_entries
 FOR EACH ROW
-WHEN NEW.parent_id IS NOT NULL AND NEW.parent_id NOT GLOB '[0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F]-[0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F]-7[0-9a-fA-F][0-9a-fA-F][0-9a-fA-F]-[89aAbB][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F]-[0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F]'
+WHEN NEW.parent_id IS NOT NULL AND NEW.parent_id NOT GLOB (SELECT pattern FROM uuid_v7_pattern LIMIT 1)
 BEGIN
     SELECT RAISE(ABORT, 'session_entries.parent_id must be NULL or a UUIDv7');
 END;
@@ -93,7 +96,7 @@ END;
 CREATE TRIGGER IF NOT EXISTS validate_session_entries_parent_id_uuid_update
 BEFORE UPDATE OF parent_id ON session_entries
 FOR EACH ROW
-WHEN NEW.parent_id IS NOT NULL AND NEW.parent_id NOT GLOB '[0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F]-[0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F]-7[0-9a-fA-F][0-9a-fA-F][0-9a-fA-F]-[89aAbB][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F]-[0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F]'
+WHEN NEW.parent_id IS NOT NULL AND NEW.parent_id NOT GLOB (SELECT pattern FROM uuid_v7_pattern LIMIT 1)
 BEGIN
     SELECT RAISE(ABORT, 'session_entries.parent_id must be NULL or a UUIDv7');
 END;
@@ -103,7 +106,7 @@ END;
 CREATE TRIGGER IF NOT EXISTS validate_session_entries_compaction_first_kept_entry_id_uuid_insert
 BEFORE INSERT ON session_entries
 FOR EACH ROW
-WHEN NEW.compaction_first_kept_entry_id <> '' AND NEW.compaction_first_kept_entry_id NOT GLOB '[0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F]-[0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F]-7[0-9a-fA-F][0-9a-fA-F][0-9a-fA-F]-[89aAbB][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F]-[0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F]'
+WHEN NEW.compaction_first_kept_entry_id <> '' AND NEW.compaction_first_kept_entry_id NOT GLOB (SELECT pattern FROM uuid_v7_pattern LIMIT 1)
 BEGIN
     SELECT RAISE(ABORT, 'session_entries.compaction_first_kept_entry_id must be empty or a UUIDv7');
 END;
@@ -113,7 +116,7 @@ END;
 CREATE TRIGGER IF NOT EXISTS validate_session_entries_compaction_first_kept_entry_id_uuid_update
 BEFORE UPDATE OF compaction_first_kept_entry_id ON session_entries
 FOR EACH ROW
-WHEN NEW.compaction_first_kept_entry_id <> '' AND NEW.compaction_first_kept_entry_id NOT GLOB '[0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F]-[0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F]-7[0-9a-fA-F][0-9a-fA-F][0-9a-fA-F]-[89aAbB][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F]-[0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F]'
+WHEN NEW.compaction_first_kept_entry_id <> '' AND NEW.compaction_first_kept_entry_id NOT GLOB (SELECT pattern FROM uuid_v7_pattern LIMIT 1)
 BEGIN
     SELECT RAISE(ABORT, 'session_entries.compaction_first_kept_entry_id must be empty or a UUIDv7');
 END;
@@ -123,7 +126,7 @@ END;
 CREATE TRIGGER IF NOT EXISTS validate_session_entries_branch_from_entry_id_uuid_insert
 BEFORE INSERT ON session_entries
 FOR EACH ROW
-WHEN NEW.branch_from_entry_id <> '' AND NEW.branch_from_entry_id NOT GLOB '[0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F]-[0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F]-7[0-9a-fA-F][0-9a-fA-F][0-9a-fA-F]-[89aAbB][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F]-[0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F]'
+WHEN NEW.branch_from_entry_id <> '' AND NEW.branch_from_entry_id NOT GLOB (SELECT pattern FROM uuid_v7_pattern LIMIT 1)
 BEGIN
     SELECT RAISE(ABORT, 'session_entries.branch_from_entry_id must be empty or a UUIDv7');
 END;
@@ -133,7 +136,7 @@ END;
 CREATE TRIGGER IF NOT EXISTS validate_session_entries_branch_from_entry_id_uuid_update
 BEFORE UPDATE OF branch_from_entry_id ON session_entries
 FOR EACH ROW
-WHEN NEW.branch_from_entry_id <> '' AND NEW.branch_from_entry_id NOT GLOB '[0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F]-[0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F]-7[0-9a-fA-F][0-9a-fA-F][0-9a-fA-F]-[89aAbB][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F]-[0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F]'
+WHEN NEW.branch_from_entry_id <> '' AND NEW.branch_from_entry_id NOT GLOB (SELECT pattern FROM uuid_v7_pattern LIMIT 1)
 BEGIN
     SELECT RAISE(ABORT, 'session_entries.branch_from_entry_id must be empty or a UUIDv7');
 END;
@@ -143,7 +146,7 @@ END;
 CREATE TRIGGER IF NOT EXISTS validate_session_messages_id_uuid_insert
 BEFORE INSERT ON session_messages
 FOR EACH ROW
-WHEN NEW.id NOT GLOB '[0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F]-[0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F]-7[0-9a-fA-F][0-9a-fA-F][0-9a-fA-F]-[89aAbB][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F]-[0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F]'
+WHEN NEW.id NOT GLOB (SELECT pattern FROM uuid_v7_pattern LIMIT 1)
 BEGIN
     SELECT RAISE(ABORT, 'session_messages.id must be a UUIDv7');
 END;
@@ -153,7 +156,7 @@ END;
 CREATE TRIGGER IF NOT EXISTS validate_session_messages_id_uuid_update
 BEFORE UPDATE OF id ON session_messages
 FOR EACH ROW
-WHEN NEW.id NOT GLOB '[0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F]-[0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F]-7[0-9a-fA-F][0-9a-fA-F][0-9a-fA-F]-[89aAbB][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F]-[0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F]'
+WHEN NEW.id NOT GLOB (SELECT pattern FROM uuid_v7_pattern LIMIT 1)
 BEGIN
     SELECT RAISE(ABORT, 'session_messages.id must be a UUIDv7');
 END;
@@ -163,7 +166,7 @@ END;
 CREATE TRIGGER IF NOT EXISTS validate_session_messages_session_id_uuid_insert
 BEFORE INSERT ON session_messages
 FOR EACH ROW
-WHEN NEW.session_id NOT GLOB '[0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F]-[0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F]-7[0-9a-fA-F][0-9a-fA-F][0-9a-fA-F]-[89aAbB][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F]-[0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F]'
+WHEN NEW.session_id NOT GLOB (SELECT pattern FROM uuid_v7_pattern LIMIT 1)
 BEGIN
     SELECT RAISE(ABORT, 'session_messages.session_id must be a UUIDv7');
 END;
@@ -173,7 +176,7 @@ END;
 CREATE TRIGGER IF NOT EXISTS validate_session_messages_session_id_uuid_update
 BEFORE UPDATE OF session_id ON session_messages
 FOR EACH ROW
-WHEN NEW.session_id NOT GLOB '[0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F]-[0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F]-7[0-9a-fA-F][0-9a-fA-F][0-9a-fA-F]-[89aAbB][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F]-[0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F]'
+WHEN NEW.session_id NOT GLOB (SELECT pattern FROM uuid_v7_pattern LIMIT 1)
 BEGIN
     SELECT RAISE(ABORT, 'session_messages.session_id must be a UUIDv7');
 END;
@@ -183,7 +186,7 @@ END;
 CREATE TRIGGER IF NOT EXISTS validate_session_messages_entry_id_uuid_insert
 BEFORE INSERT ON session_messages
 FOR EACH ROW
-WHEN NEW.entry_id NOT GLOB '[0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F]-[0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F]-7[0-9a-fA-F][0-9a-fA-F][0-9a-fA-F]-[89aAbB][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F]-[0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F]'
+WHEN NEW.entry_id NOT GLOB (SELECT pattern FROM uuid_v7_pattern LIMIT 1)
 BEGIN
     SELECT RAISE(ABORT, 'session_messages.entry_id must be a UUIDv7');
 END;
@@ -193,13 +196,14 @@ END;
 CREATE TRIGGER IF NOT EXISTS validate_session_messages_entry_id_uuid_update
 BEFORE UPDATE OF entry_id ON session_messages
 FOR EACH ROW
-WHEN NEW.entry_id NOT GLOB '[0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F]-[0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F]-7[0-9a-fA-F][0-9a-fA-F][0-9a-fA-F]-[89aAbB][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F]-[0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F]'
+WHEN NEW.entry_id NOT GLOB (SELECT pattern FROM uuid_v7_pattern LIMIT 1)
 BEGIN
     SELECT RAISE(ABORT, 'session_messages.entry_id must be a UUIDv7');
 END;
 -- +goose StatementEnd
 
 -- +goose Down
+DROP TABLE IF EXISTS uuid_v7_pattern;
 DROP TRIGGER IF EXISTS validate_session_messages_entry_id_uuid_update;
 DROP TRIGGER IF EXISTS validate_session_messages_entry_id_uuid_insert;
 DROP TRIGGER IF EXISTS validate_session_messages_session_id_uuid_update;
