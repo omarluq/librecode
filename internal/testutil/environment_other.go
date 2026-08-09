9c514556f428a88d93832391b3fe3bd1cf5eba3f
Omar Alani <omar.luqman@hey.com>
2026-08-09 12:10:31 -0500

    test: cover remaining Windows home paths

--- internal/testutil/environment_other.go
+++ internal/testutil/environment_other.go
@@ -4,4 +4,6 @@ package testutil
 
 import "testing"
 
-func setWindowsHome(_ *testing.T, _ string) {}
+func setWindowsHome(_ *testing.T, _ string) {
+	// Non-Windows platforms do not use the Windows-specific home variables.
+}
